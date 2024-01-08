091199c8fae98fee7fff628833c6c5162bb68cc3
Valentin Krajzelman <valentinkrajzelman@hotmail.com>
2024-01-08 14:08:19 -0300

    cosas

--- src/api/novedades.js
+++ src/api/novedades.js
@@ -5,6 +5,6 @@ const url = 'https://mmxapp2.mercomaxsa.com.ar/node/rhNovedades/';
 export const fetchCurrentNovedades = () => axios.get(url + "current");
 // export const fetchOneDetalles = (id) => axios.get(url+'one',id);
 // export const fetchManyDetalles = (id) => axios.get(url+'many/' + id);
-// export const fetchPostDetalles = (detalle) => axios.post(url+'one',detalle);
+export const fetchPostNovedades = (novedades) => axios.post(url+'one',novedades);
 // export const fetchUpdateDetalles = (detalle) => axios.post(url+'updateOne',detalle);
 // export const fetchDeleteDetalles = (id) => axios.delete(url+'one/'+id);
